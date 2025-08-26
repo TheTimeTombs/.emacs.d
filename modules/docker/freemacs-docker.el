80eb9ebd965e40105182c1bafcbaacfdcf3cee53
Thomas <thomas@pidesktop>
2025-08-26 08:26:50 -0400

    Ensure docker package

--- modules/docker/freemacs-docker.el
+++ modules/docker/freemacs-docker.el
@@ -1,6 +1,7 @@
 ;; -*- lexical-binding: t -*-
 
 (use-package docker
+  :ensure t
   :commands docker
   :init
   (trapt-exec-find "docker"
@@ -8,9 +9,11 @@
   :bind ("C-c d" . docker))
 
 (use-package docker-compose-mode
+  :ensure t
   :mode "docker-compose\\.y\.")
 
 (use-package dockerfile-mode
+  :ensure t
   :mode ("Dockerfile\\'" . dockerfile-mode))
 
 (provide 'freemacs-docker)
